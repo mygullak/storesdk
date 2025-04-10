b5ff8f1cf78d45e552b7abe8cf1501fcae964d9b
yashhbl <114727043+yashhbl@users.noreply.github.com>
2025-04-10 15:21:08 +0530

    modify-status-bar-to-white (#7)
    * modify-status-bar-to-white
    
    * change statusBarColor back to original

--- store-sdk/src/main/java/money/myhubble/storesdk/HubbleActivity.kt
+++ store-sdk/src/main/java/money/myhubble/storesdk/HubbleActivity.kt
@@ -1,16 +1,29 @@
 package money.myhubble.storesdk
 
+import android.R
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
+
 
 class HubbleActivity : AppCompatActivity() {
 
     private lateinit var hubbleFragment: HubbleFragment
     private var hubbleFragmentTag = "hubble-webview-fragment"
+    private var defaultStatusBarColor = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        val window = window
+        defaultStatusBarColor = window.statusBarColor
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.isAppearanceLightStatusBars = true
 
         hubbleFragment = Hubble.getFragment()
 
@@ -32,5 +45,13 @@ class HubbleActivity : AppCompatActivity() {
         }
 
         return super.onKeyDown(keyCode, eventName);
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val window = window
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        window.statusBarColor = defaultStatusBarColor;
+        windowInsetsController.isAppearanceLightStatusBars = false
     }
 }
